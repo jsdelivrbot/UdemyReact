0fb37abbb6fbde8e89590604f15fa2fa41f9e879
andrewtdinh <andrewdinh74@yahoo.com>
2016-10-22 22:10:11 -0700

    import SearchBar component into index.js

--- project1/src/index.js
+++ project1/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = process.env.YOUTUBE_API_KEY;
 
 const App = () => {
