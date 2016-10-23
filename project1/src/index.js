65a403dd8d790ccce344e0b00ebf4587faca18e7
andrewtdinh <andrewdinh74@yahoo.com>
2016-10-23 14:46:36 -0700

    Refactor App into a class component

--- project1/src/index.js
+++ project1/src/index.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+// const API_KEY = process.env.YOUTUBE_API_KEY;
+const API_KEY = 'AIzaSyC-RDyK8WQlhFZ7rLA9M3QWAvDKAfuXD6E';
 
-const API_KEY = process.env.YOUTUBE_API_KEY;
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+  console.log(data);
+});
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
