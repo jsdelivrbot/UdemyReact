4a649f710339cdee7a580308c292bad7793e7ffa
andrewtdinh <andrewdinh74@yahoo.com>
2016-10-27 22:19:35 -0700

    Create a constant for image url

--- project1/src/components/video_list_item.js
+++ project1/src/components/video_list_item.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const VideoListItem = ({video}) => {
-  return <li>Video</li>
+  const imageUrl = video.snippet.thumbnails.default.url;
+
+  return (
+    <li className='list-group-item'>
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object"/>
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading">
+
+          </div>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default VideoListItem;
