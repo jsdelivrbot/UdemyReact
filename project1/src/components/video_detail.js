146d01dd67252a3b287ca0f913aae0dc9bf83315
andrewtdinh <andrewdinh74@yahoo.com>
2016-10-29 22:38:49 -0700

    Add iframe inside VideoDetail

--- project1/src/components/video_detail.js
+++ project1/src/components/video_detail.js
@@ -4,7 +4,7 @@ const VideoDetail = (props) => {
   return (
     <div className="video-detail col-md-8">
       <div className="embed-responsive embed-responsive-16by9">
-
+        <iframe className="embed-responsive-item"></iframe>
       </div>
     </div>
   )
