5abfb4bcc5103ba48ac7a5955ddc703e7036d070
andrewtdinh <andrewdinh74@yahoo.com>
2016-10-29 22:41:17 -0700

    Build up VideoDetail

--- project1/src/components/video_detail.js
+++ project1/src/components/video_detail.js
@@ -6,6 +6,10 @@ const VideoDetail = (props) => {
       <div className="embed-responsive embed-responsive-16by9">
         <iframe className="embed-responsive-item"></iframe>
       </div>
+      <div className="details">
+        <div>Title</div>
+        <div>Description</div>
+      </div>
     </div>
   )
 }
