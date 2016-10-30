9ae41d28930b97d046823dd3fa7c8363b91fa26b
andrewtdinh <andrewdinh74@yahoo.com>
2016-10-29 22:35:50 -0700

    Build up VideoDetail

--- project1/src/components/video_detail.js
+++ project1/src/components/video_detail.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const VideoDetail = (props) => {
+  return (
+    <div className="video-detail col-md-8">
+      <div className="embed-responsive embed-responsive-16by9">
 
+      </div>
+    </div>
+  )
 }
 
 export default VideoDetail;
