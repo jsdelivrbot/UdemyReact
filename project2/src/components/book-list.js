6a35215567f2204dfce9a21a30855bc793cc601a
andrewtdinh <andrewdinh74@yahoo.com>
2016-11-12 19:30:37 -0800

    Build out BookList

--- project2/src/components/book-list.js
+++ project2/src/components/book-list.js
@@ -2,4 +2,12 @@ import React, { Component } from 'react';
 
 export default class BookList extends Component {
   
+
+  render() {
+    return (
+      <ul className='list-group col-sm-4'>
+        {this.renderList()}
+      </ul>
+    )
+  }
 }
