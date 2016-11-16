e27ab3296dbcb84aebb68da72618bcaa237d6d15
andrewtdinh <andrewdinh74@yahoo.com>
2016-11-15 21:55:06 -0800

    Create mapDispatchToProps

--- project2/src/containers/book-list.js
+++ project2/src/containers/book-list.js
@@ -30,4 +30,8 @@ function mapStateToProps(state) {
   }
 }
 
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({ selectBook: selectBook}, dispatch);
+}
+
 export default connect(mapStateToProps)(BookList);
