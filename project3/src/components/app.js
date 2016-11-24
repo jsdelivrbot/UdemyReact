6ab4c58cf47928ef72eb4793aaf6e9a3b75144a6
andrewtdinh <andrewdinh74@yahoo.com>
2016-11-23 23:55:25 -0800

    Add a button into the form

--- project3/src/components/app.js
+++ project3/src/components/app.js
@@ -4,8 +4,11 @@ import SearchBar from '../containers/search_bar'
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter
+      <div>
         <SearchBar/>
+        <span className='input-group-btn'>
+          <button className="btn btn-secondary">Submit</button>
+        </span>
       </div>
     );
   }
