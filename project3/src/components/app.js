73a625f2062204dec8f0233dbc6638b5281de9a7
andrewtdinh <andrewdinh74@yahoo.com>
2016-11-24 23:33:46 -0800

    Add constructor to SearchBar class

--- project3/src/components/app.js
+++ project3/src/components/app.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import SearchBar from '../containers/search_bar'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {term: ''};
+  }
+
   render() {
     return (
       <div>
