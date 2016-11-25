6a75257b8f045e4fa1dd38c5d2bbe553c0b7e558
andrewtdinh <andrewdinh74@yahoo.com>
2016-11-24 23:46:39 -0800

    Move code into correct files

--- project3/src/components/app.js
+++ project3/src/components/app.js
@@ -2,19 +2,10 @@ import React, { Component } from 'react';
 import SearchBar from '../containers/search_bar'
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {term: ''};
-  }
-
   render() {
     return (
       <div>
         <SearchBar/>
-        <span className='input-group-btn'>
-          <button type='submit' className="btn btn-secondary">Submit</button>
-        </span>
       </div>
     );
   }
