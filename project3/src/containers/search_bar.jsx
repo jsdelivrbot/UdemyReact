a775386a43d302c96216ff3aff9c827fc8b650d9
andrewtdinh <andrewdinh74@yahoo.com>
2016-11-24 23:49:45 -0800

    Add properties to the input

--- project3/src/containers/search_bar.jsx
+++ project3/src/containers/search_bar.jsx
@@ -10,7 +10,11 @@ export default class SearchBar extends Component {
   render() {
     return (
       <form className="input-group">
-        <input />
+        <input
+          placeholder='Get a five-day forcast in your favorite cities'
+          className='form-control'
+          value={this.state.term}
+          onChange={this.onInputChange}/>
         <span className='input-group-btn'>
           <button type='submit' className="btn btn-secondary">Submit</button>
         </span>
