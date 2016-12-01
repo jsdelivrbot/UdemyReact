fd6936154df2dd79342bfc13b9422f82b8925fcb
andrewtdinh <andrewdinh74@yahoo.com>
2016-11-30 23:04:16 -0800

    Create Root URL for making request

--- project3/src/actions/index.js
+++ project3/src/actions/index.js
@@ -1,4 +1,5 @@
 const API_KEY = 'd8bc0370aebef0d539ec1991485bb6c5';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
