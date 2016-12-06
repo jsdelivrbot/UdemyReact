042dc09ef2f462d7e47e6935e63bc1eeb4019184
andrewtdinh <andrewdinh74@yahoo.com>
2016-12-05 23:58:45 -0800

    Resolved issues not getting response from api

--- project3/src/actions/index.js
+++ project3/src/actions/index.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const API_KEY = 'd8bc0370aebef0d539ec1991485bb6c5';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+// const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?q=London,us&mode=json&appid=d8bc0370aebef0d539ec1991485bb6c5`;
+
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
