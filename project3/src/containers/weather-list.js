50232258f9ecb02973518c0ae15313997d8258ee
andrewtdinh <andrewdinh74@yahoo.com>
2016-12-14 23:55:06 -0800

    Try displaying the list of cities first

--- project3/src/containers/weather-list.js
+++ project3/src/containers/weather-list.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class WeatherList extends Component {
+  renderWeather(cityData){
+    return (
+      <tr>
+        <td>{cityData.city.name}</td>
+      </tr>
+    )
+  }
   render(){
     return (
       <table className="table table-hover">
