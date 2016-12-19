19e7225b181fc09c3835dd65d445639604cf9dfa
andrewtdinh <andrewdinh74@yahoo.com>
2016-12-19 00:27:18 -0800

    Add the average beneath the charts

--- project3/src/components/chart.js
+++ project3/src/components/chart.js
@@ -8,6 +8,7 @@ export default (props) => {
         <SparklinesLine color={props.color}/>
         <SparklinesReferenceLine type="avg"/>
       </Sparklines>
+      <div>{average(props.data)}</div>
     </div>
   )
 }
