c379bc011d432cf2c19dd5670c6a637c9ca7b281
Andrew Dinh <adinh@sightmachine.com>
2017-01-31 23:26:59 -0800

    Add index route

--- project4/src/routes.js
+++ project4/src/routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
+import PostsIndex from './components/posts_index';
 
 import App from './components/app';
 
@@ -9,6 +10,7 @@ const Greeting = () => {
 
 export default(
   <Route path='/' component={App} >
+    <IndexRoute component={PostsIndex} />
     <Route path='greet' component={Greeting}/>
     <Route path='greet2' component={Greeting}/>
     <Route path='greet3' component={Greeting}/>
