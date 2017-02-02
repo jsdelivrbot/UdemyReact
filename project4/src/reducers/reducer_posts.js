e3b0973dce192f5e70cfcb887ebd13291588a6ff
Andrew Dinh <adinh@sightmachine.com>
2017-02-02 14:45:18 -0800

    Adding initial state to the reducer.

--- project4/src/reducers/reducer_posts.js
+++ project4/src/reducers/reducer_posts.js
@@ -1,7 +1,10 @@
-const INITIAL_STATE = {};
+import { FETCH_POSTS } from '../actions/index';
+
+const INITIAL_STATE = { all: [], post: null };
 
 export default function(state = INITIAL_STATE, action) {
   switch (action.type) {
+  case FETCH_POSTS:
   default:
       return state;
   }
