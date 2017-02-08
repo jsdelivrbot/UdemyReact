f828fc2f6469e6f16d575310d2363f4a55ac09ae
Andrew Dinh <adinh@sightmachine.com>
2017-02-07 18:18:27 -0800

    Introduce componentWillMount function

--- project4/src/components/posts_index.js
+++ project4/src/components/posts_index.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 class PostsIndex extends Component {
+  componentWillMount () {
+    console.log('This would be a good time to call an action creator to fetch posts.')
+  }
+
   render () {
     return (
       <div>List of blog posts</div>
