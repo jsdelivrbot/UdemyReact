ce8a8142ae92a298a307d081633363873d99a916
Andrew Dinh <adinh@sightmachine.com>
2017-03-10 00:25:15 -0800

    Refactor for brevity

--- project4/src/components/posts_index.js
+++ project4/src/components/posts_index.js
@@ -15,8 +15,4 @@ class PostsIndex extends Component {
   }
 }
 
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({fetchPosts}, dispatch);
-}
-
-export default connect(null, mapDispatchToProps)(PostsIndex);
+export default connect(null, {fetchPosts})(PostsIndex);
