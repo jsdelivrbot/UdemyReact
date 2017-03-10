e44d1572db9cd3dae6f308b8ac4a0ac5e4d15dab
Andrew Dinh <adinh@sightmachine.com>
2017-03-09 23:56:57 -0800

    Bring in bindActionCreators

--- project4/src/components/posts_index.js
+++ project4/src/components/posts_index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { fetchPosts } from '../actions/index';
 
 class PostsIndex extends Component {
