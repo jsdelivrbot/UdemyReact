0088782a6c9c381ddbd39f391978a2093b83cdb2
Andrew Dinh <adinh@sightmachine.com>
2017-03-13 00:17:20 -0700

    Building title input

--- project4/src/components/posts_new.js
+++ project4/src/components/posts_new.js
@@ -4,7 +4,13 @@ import { reduxForm } from 'redux-form';
 class PostsNew extends Component {
   render() {
     return (
-      <div>Create Form</div>
+      <form action="">
+        <h3>Create A New Post</h3>
+        <div className='form-group'>
+          <label>Title</label>
+          <input type="text" className='form-control'/>
+        </div>
+      </form>
     );
   }
 }
