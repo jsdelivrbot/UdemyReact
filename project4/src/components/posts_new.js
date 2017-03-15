58ebe919ccbdd00fd517452098d2f844806a725a
Andrew Dinh <adinh@sightmachine.com>
2017-03-14 17:57:58 -0700

    Add configuration objects into the form

--- project4/src/components/posts_new.js
+++ project4/src/components/posts_new.js
@@ -10,17 +10,17 @@ class PostsNew extends Component {
         <h3>Create A New Post</h3>
         <div className='form-group'>
           <label>Title</label>
-          <input type="text" className='form-control'/>
+          <input type="text" className='form-control' {...title}/>
         </div>
 
         <div className='form-group'>
           <label>Categories</label>
-          <input type="text" className='form-control'/>
+          <input type="text" className='form-control' {...categories}/>
         </div>
 
         <div className='form-group'>
           <label>Content</label>
-          <textarea type="text" className='form-control'/>
+          <textarea type="text" className='form-control' {...content}/>
         </div>
 
         <button type='submit' className='btn btn-primary'>Submit</button>
