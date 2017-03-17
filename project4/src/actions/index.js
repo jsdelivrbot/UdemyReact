37e24df8a4385c171b41ab2abcf87009e2e8d96a
Andrew Dinh <adinh@sightmachine.com>
2017-03-16 23:55:30 -0700

    Forgot to add the payload to post

--- project4/src/actions/index.js
+++ project4/src/actions/index.js
@@ -16,7 +16,7 @@ export function fetchPosts() {
 }
 
 export function createPost(props){
-  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`);
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
 
   return {
     type:  CREATE_POST,
