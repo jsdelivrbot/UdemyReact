dce0ac58cf256df8f9c989d7481f4742af4da187
Andrew Dinh <adinh@sightmachine.com>
2017-03-24 00:09:38 -0700

    Render the error messages on the form

--- project4/src/components/posts_new.js
+++ project4/src/components/posts_new.js
@@ -20,11 +20,17 @@ class PostsNew extends Component {
         <div className='form-group'>
           <label>Categories</label>
           <input type="text" className='form-control' {...categories}/>
+          <div className='text-help'>
+            {categories.touched ? categories.error : ''}
+          </div>
         </div>
 
         <div className='form-group'>
           <label>Content</label>
           <textarea type="text" className='form-control' {...content}/>
+          <div className='text-help'>
+            {content.touched ? content.error : ''}
+          </div>
         </div>
 
         <button type='submit' className='btn btn-primary'>Submit</button>
