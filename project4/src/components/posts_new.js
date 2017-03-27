7d76bedb487f33b3bb82fcef9d4edd32aa1d6c3f
Andrew Dinh <adinh@sightmachine.com>
2017-03-26 22:15:18 -0700

    Import PropTypes

--- project4/src/components/posts_new.js
+++ project4/src/components/posts_new.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
 import { Link } from 'react-router';
