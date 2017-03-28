af5b78a66281ca09ef8504278b5f45283585fc93
Andrew Dinh <adinh@sightmachine.com>
2017-03-28 00:01:23 -0700

    Add comment

--- project4/src/components/posts_new.js
+++ project4/src/components/posts_new.js
@@ -11,7 +11,10 @@ class PostsNew extends Component {
   onSubmit(props){
     this.props.createPost(props)
       .then(() => {
-        //
+        //blog post has been created, navigate user to the index
+        //We navigate by calling this.context.router.push with the
+        //new path to navigate to.
+        
       });
   }
 
