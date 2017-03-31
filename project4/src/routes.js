08bfe4f91a1d07367c2a1d7f003a9924d5c1a8e6
Andrew Dinh <adinh@sightmachine.com>
2017-03-31 00:15:28 -0700

    Import postsShow

--- project4/src/routes.js
+++ project4/src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 
 import App from './components/app';
 
