cfcd7f0826a16b343e5bb9464d03272389988b41
Andrew Dinh <adinh@sightmachine.com>
2018-01-16 23:45:02 -0800

    Add react router

--- blog/src/components/app.js
+++ blog/src/components/app.js
@@ -1,12 +1,9 @@
-import React from 'react';
-import { Component } from 'react';
+import React, { Component } from 'react';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        {this.props.children}
-      </div>
+      <div>React simple starter</div>
     );
   }
 }
--- project4/src/components/posts_show.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class PostsShow extends Component {
-  render() {
-    return <div>Show post {this.props.params.id}</div>
-  }
-}
-
-export default PostsShow;
