e2ef795be4c6453ce29321b8c713a7b0d06dfc8f
Andrew Dinh <adinh@sightmachine.com>
2018-01-29 09:26:45 -0800

    Introduce componentWillMount to fetch posts into posts index

--- blog/src/components/posts_index.js
+++ blog/src/components/posts_index.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 class PostsIndex extends Component {
+  componentWillMount() {
+    console.log('This is where to call the action to fetch posts');
+  }
+
   render() {
     return (
       <div>List of blog posts</div>
