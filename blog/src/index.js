89f963d964bccfaed853e80c829b6e5580637aa2
Andrew Dinh <adinh@sightmachine.com>
2018-02-12 09:37:27 -0800

    Switch from browserHistory to hashHistory to get rid of warning

--- blog/src/index.js
+++ blog/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { Router, browerHistory } from 'react-router';
+import { Router, hashHistory } from 'react-router';
 import routes from './routes';
 import promise from 'redux-promise';
 
@@ -14,6 +14,6 @@ const createStoreWithMiddleware = applyMiddleware(
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={browerHistory} routes={routes} />
+    <Router history={hashHistory} routes={routes} />
   </Provider>
   , document.querySelector('.container'));
